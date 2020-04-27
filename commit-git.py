ab3b741420f7437d13c3a219a147b2bf340a4095
bucomar <bucomar@gmail.com>
2020-04-28 00:19:19 +0200

    nyuuu worzson

--- commit-git.py
+++ commit-git.py
@@ -1,8 +1,28 @@
+#import  git
+
+#repo = '/storage/emulated/0/git/git/.git'  # make sure .git folder is properly configured
+#comtext = 'nyuuu vorzson'
+
+#def git_push():
+#    try:
+#        git.Repo = Repo(repo)
+#        git.Repo.git.add(update=True)
+#        git.Repo.index.commit(comtext)
+#        origin = git.Repo.remote(name='origin')
+        #origin.push()
+    #except:
+#        print('Some error occured while pushing the code')    
+
+#git_push()
+
+
+
 import subprocess as cmd
 
 cmd.run('cd /storage/emulated/0/git/git', check=True, shell=True)
 cmd.run('pwd', check=True, shell=True)
 cmd.run('ls', check=True, shell=True)
+cmd.run('git status', check=True, shell=True)
 cmd.run('git add .', check=True, shell=True)
-cmd.run('git commit -m "nyuuu wörzsön" ', check=True, shell=True)
+cmd.run('git commit -m "nyuuu worzson" ', check=True, shell=True)
 cmd.run('git push', check=True, shell=True)
