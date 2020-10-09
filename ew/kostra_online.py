938c3ad59fee58acba245314e0c7bc3652ab40ac
bucomar <bucomar@gmail.com>
2020-10-10 00:17:02 +0200

    KOSTRA ZIP mentés nélkül

---
+++ ew/kostra_online.py
@@ -0,0 +1,91 @@
+""" NiderschlagSpendenDaten von KOSTRA """
+
+## import modules
+
+# from zipfile import ZipFile as zp
+import requests, zipfile, io
+import pandas as pd
+
+##	make index_rc
+
+# COORD_B = 53.5928618
+# COORD_L = 9.4709494
+
+############################################
+### KOSTRA Koordinaten hier schreiben!!! ###
+############################################
+COL = 68 ###################################
+ROW = 20 ###################################
+############################################
+
+############################################
+
+I_RC = int(str(COL) + '0' + str(ROW))
+print ('col: ', COL, '\n', 'row: ', ROW, '\n', 'index_rc: ', I_RC)
+
+##	url list
+REGEN_DAUER = [
+'0005', '0010', '0015', '0020', '0030', '0045', '0060', '0090', '0120', '0180', '0240', '0360', '0540', '0720', '1080', '1440', '2880', '4320'
+]
+
+URL = []
+
+for i in range(len(REGEN_DAUER)):
+	URL.append(
+	'https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/asc/StatRR_KOSTRA-DWD-2010R_D'
+	+REGEN_DAUER[i]
+	+'.csv.zip'
+	)
+
+## Functionen
+
+def csv_name(nro):
+	""" csv name generator """
+	csv_nm = str(
+	'StatRR_KOSTRA-DWD-2010R_D'+str(nro)+'.csv')
+	return csv_nm
+
+def df_name(nro):
+	""" df name generator """
+	df_nm = str('df'+str(nro)+'.csv')
+	return df_nm
+
+def get_csv_df(pos):
+	""" get csv-s to df """
+	print(pos)
+	r = requests.get(URL[pos])
+	z = zipfile.ZipFile(io.BytesIO(r.content))
+	csv = z.open(csv_name(REGEN_DAUER[pos]))
+	df = pd.read_csv(csv, sep = ';', index_col=('INDEX_RC'))
+	return df
+
+def df_row_exp(from_df, rc_i, to_df, pos):
+	""" ##	row export to INDEX_RC """
+	# to_df = pd.concat([to_df, from_df.loc[from_df['INDEX_RC'] == int(rc_index)]])
+	a_row = from_df.loc[[rc_i]]
+	a_row = a_row.apply(lambda i: round(i*100*100/60/int(REGEN_DAUER[pos]), 2))
+	# print(a_row)
+	# print()
+	to_df = pd.concat([to_df, a_row])
+	# print(to_df)
+	# print()
+	return to_df
+
+##	make local_RS
+
+LOCAL_RS = pd.DataFrame()
+
+for i in range(0, len(REGEN_DAUER)):
+	DF_TEMP = get_csv_df(i)
+	LOCAL_RS = df_row_exp(DF_TEMP, I_RC, LOCAL_RS, i)
+
+LOCAL_RS.index = REGEN_DAUER
+LOCAL_RS.index.name = 'Regendauer'
+LOCAL_RS.columns.name = 'Regenhaufigkeit'
+
+print(LOCAL_RS)
+
+LOCAL_RS_FILE_NAME = str('kostra_' + str(I_RC) + '_l.csv')
+LOCAL_RS.to_csv(LOCAL_RS_FILE_NAME, sep='\t', decimal=',')
+
+print(LOCAL_RS_FILE_NAME + ' ist fertig!')
--- ew/kostra_sandbox.py
+++ ew/kostra_sandbox.py
@@ -1,79 +1,20 @@
+import requests, zipfile, io
 import pandas as pd
-import zipfile as zip
 
-# my_zipfile = zip.ZipFile('ew/StatRR_KOSTRA-DWD-2010R_D0005.csv.zip')
-my_zipfile = zip.ZipFile('https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/asc/StatRR_KOSTRA-DWD-2010R_D0005.csv.zip')
-my_zipfile.extractall()
-# https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/asc/StatRR_KOSTRA-DWD-2010R_D0005.csv.zip
+URL='https://opendata.dwd.de/climate_environment/CDC/grids_germany/return_periods/precipitation/KOSTRA/KOSTRA_DWD_2010R/asc/StatRR_KOSTRA-DWD-2010R_D0005.csv.zip'
 
+def get_csv_df(url):
+    print('Türelem! Dolgozok')
+    r = requests.get(URL)
+    type(r)
 
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    type(z)
 
+    csv = z.open('StatRR_KOSTRA-DWD-2010R_D0005.csv')
+    type(csv)
 
-LOCAL_RS = pd.read_csv('ew/kostra_21031.csv', sep=',', index_col=('Regendauer'))
-print(LOCAL_RS)
-# LOCAL_RS.to_excel('kostra_21031.xls')
+    df = pd.read_csv(csv)
+    return df
 
-
-# def mm_to_lsha(df):
-
-a_row = LOCAL_RS.loc[[5]]
-print(a_row)
-
-a_row = a_row.apply(lambda i: i*100*100/60/5)
-print(a_row)
-
-I_RC = 21031
-
-# tab1 = pd.DataFrame([
-# [1, 2, 3],
-# [4, 5, 6,],
-# [7, 8, 9]
-# ], index=('t', 'a', 'b'))
-# print(tab)
-#
-# tab1 = pd.DataFrame([[1, 2, 3]])
-# print(tab1)
-# print()
-# tab2 = pd.DataFrame([[4, 5, 6]])
-# print(tab2)
-# print()
-# tab1 = pd.concat([tab1, tab2])
-# print(tab1)
-
-# index = [tab.index]
-# print(index)
-# tab.index(['t', 'a', 'b'])
-
-# print(tab)
-
-df = pd.DataFrame()
-
-df1 = pd.read_csv('temp/StatRR_KOSTRA-DWD-2010R_D0005.csv', sep=';', index_col='INDEX_RC')
-df2 = pd.read_csv('temp/StatRR_KOSTRA-DWD-2010R_D0010.csv', sep=';', index_col='INDEX_RC')
-df3 = pd.read_csv('temp/StatRR_KOSTRA-DWD-2010R_D0015.csv', sep=';', index_col='INDEX_RC')
-# print(table.loc[table['INDEX_RC'] == 21031])
-# print(df.index)
-# print(df.iloc[5:10, :])
-# print(df.tail())
-# df.info()
-row1 = df1.loc[[I_RC]]
-print(row1)
-print()
-df = pd.concat([df, row1])
-print(df)
-print()
-
-row2 = df2.loc[[I_RC]]
-print(row2)
-print()
-df = pd.concat([df, row2])
-print(df)
-print()
-
-
-row3 = df3.loc[[I_RC]]
-print(row1)
-df = pd.concat([df, row3])
-print()
-print(df)
-print()
+get_csv_df(URL)
