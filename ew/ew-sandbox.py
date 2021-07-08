dd581cc2756b175396f328a2abbc64fb6513f9df
bucomar <bucomar@gmail.com>
2021-07-08 21:12:50 +0200

    sandbox - du-sum meg nem az igazi

--- ew/ew-sandbox.py
+++ ew/ew-sandbox.py
@@ -3,41 +3,64 @@ import pandas as pd
 import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
-#import latex
-import ew_fx as fx
+import latex import ew_fx as fx
 
 ##
 
-sanyter_df = pd.read_csv('ew_data/sanyter.csv', sep='|', index_col=0)
+sanyter_df = pd.read_csv('ew_data/sanyter.csv', sep='|')
 
-columns = ['ID'] + list(sanyter_df.index)
-print(columns)
+print(sanyter_df)
 
+k = 0.5
 ##
 
-df = pd.read_csv('ew_in/0001_sw_input.csv', sep='|', index_col=1)
+ew_sw = pd.read_csv('ew_in/0001_sw_input.csv', sep='|')
+ew_sw['id'] = ew_sw['id'].astype(str)
 
-print(df)
-
-##
-
-
-
-
+print(ew_sw)
 
 ##
 
 
-C
-T
-D
-M
-K
-G
+ew_sw = pd.merge(left=ew_sw, right=sanyter_df, how='outer', on='san')
+ew_sw = ew_sw.sort_values(by='id')
 
-wc
-wt
-du
-wm
-ks
-gs
+print(ew_sw)
+
+##
+
+#print(ew_sw['DU_SUM'])
+#print(type(ew_sw['DU_SUM']))
+ew_sw['Q_ww'] = fx.q_ww(k, ew_sw['du']) 
+ew_sw['Q_P'] = 0
+ew_sw['Q_C'] = 0
+
+ew_sw['Q_tot'] = fx.q_tot(ew_sw['Q_ww'], ew_sw['Q_C'], ew_sw['Q_P'])
+
+ew_sw['Q_zul'] = 0
+
+print(ew_sw)
+
+##
+
+
+
+
+
+
+
+##
+
+
+
+
+
+
+
+
+##
+
+
+
+
+
